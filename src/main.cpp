e91efa7572f02f34ebedac738188ab3af7856821
Thomas Duckworth <tduck@filotimoproject.org>
2025-05-13 11:31:18 +0000

    Chore: License things correctly

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+/*
+    SPDX-License-Identifier: GPL-3.0-or-later
+    SPDX-FileCopyrightText: 2025 Denys Madureira <denysmb@zoho.com>
+    SPDX-FileCopyrightText: 2025 Thomas Duckworth <tduck@filotimoproject.org>
+*/
+
 #include "distroboxmanager.h"
 #include "version-kontainer.h"
 #include <KAboutData>
